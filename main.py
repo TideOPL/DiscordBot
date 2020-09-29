45d919039631e6f4268b11d3e3875461a6ce899b
TideOPL <57710757+TideOPL@users.noreply.github.com>
2020-09-29 22:07:28 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
 import random
 
+repeat = input("(!) Would you like to initialize the game loop? (Yes or No): ")
+
+if repeat == "Yes":
+    print("(!) The game will now repeat!")
+    running = True
+
+
+def maingameloop():
+    play = input("Would you like to roll? (Yes or No): ")
+
+    if play == "Yes":
+        print("(!) Rolling!")
+
+
+
+
+while running:
